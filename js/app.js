7c769738d6d59d7816ce0bf33ef31340a58c6f22
“AndreaFerreiro” <andreafga28@gmail.com>
2023-11-18 23:31:10 +0100

    finish game

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const bird = document.querySelector('.bird')
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground')
-    const startBtn = document.querySelector('.startBtn')
     const score = document.querySelector('score')
     let birdLeft = 220
     let birdBottom = 100
     let gravity = 2
     let isGameOver = false
     let gap = 430
+    let gameTimerId
     function startGame () {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft +'px'
     }
-    let gameTimerId = setInterval(startGame, 20)
-
+    
+    gameTimerId = setInterval(startGame, 20)
     function control (e) {
         if (e.keyCode === 32) {
             jump()
